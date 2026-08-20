a218f59a978b146a2983f1db5cfe385af5920cc3
Stephan <Stephan.Goergens@bmw.de>
2026-08-20 16:26:25 +0200

    Initial Commit

---
+++ TestSplitView/TestSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSplitViewApp.swift
+//  TestSplitView
+//
+//  Created by Stephan Goergens on 20.08.26.
+//
+
+import SwiftUI
+
+@main
+struct TestSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
